1b94cf696a13a666ff465212094bed2f5f66a531
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-08 21:10:54 +0530

    changes due to destructring and short arrow fn.

--- part01/part1/src/App.jsx
+++ part01/part1/src/App.jsx
@@ -1,53 +1,26 @@
-const Hello = (props) => {
-  console.log(props);
+const Hello = ({ name, age }) => {
+  // Component helper function
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old.
+        Hello {name}, you are {age} years old.
       </p>
-    </div>
-  );
-};
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by{" "}
-      <a href="https://github.com/wasiqurzaman">Wasiqur Zaman</a>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
 
 const App = () => {
-  // console.log("Hello form component");
-  // const now = new Date();
-  // const a = 10;
-  // const b = 20;
-  // console.log(a + b);
-
   const name = "Peter";
   const age = 10;
-
-  const friends = [
-    { name: "Maya", age: 12 },
-    { name: "Peter", age: 8 },
-  ];
-
-  const boys = ["Peter", "James"];
 
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="George" age={26 + 10} />
       <Hello name={name} age={age} />
-      <p>
-        {friends[0].name} {friends[0].age}
-      </p>
-      <p>
-        {friends[1].name} {friends[1].age}
-      </p>
-      <p>{boys}</p>
-      <Footer />
     </div>
   );
 };
