f136066a8ba3264c40379921c3564419d734e079
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-10 02:57:41 +0530

    anecdotes: step 1, 2 & 3 done

--- part01/anecdotes/src/App.jsx
+++ part01/anecdotes/src/App.jsx
@@ -1,3 +1,49 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>anecdotes</div>;
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(8).fill(0));
+
+  const randomNum = () => {
+    return Math.floor(Math.random() * 8);
+  };
+
+  const handleAnecdotesChange = () => {
+    const num = randomNum();
+    console.log("current anecdote index: ", num);
+    setSelected(num);
+  };
+
+  const mostVoted = votes.indexOf(Math.max(...votes));
+  console.log("most voted anecdote index: ", mostVoted);
+
+  const handleVoteChange = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes of the day</h2>
+      <div>{anecdotes[selected]}</div>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVoteChange}>vote</button>
+      <button onClick={handleAnecdotesChange}>next anecdotes</button>
+      <h2>Anecdotes with most votes</h2>
+      <div>{anecdotes[mostVoted]}</div>
+    </div>
+  );
 };
+
+export default App;
