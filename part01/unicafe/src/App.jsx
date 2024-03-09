117305312a0133ac1c4f518ed8234ff118ce0ef9
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-09 18:15:00 +0530

    unicafe: step1 done

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <div>
-      <h1>Unicafe</h1>
+      <h2>Give geedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 };
