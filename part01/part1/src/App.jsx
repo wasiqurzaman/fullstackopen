eb94a35c51fa4c7adc2503e3494a5c168c95be60
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-09 17:45:18 +0530

    necessary changes to follow along tutorial

--- part01/part1/src/App.jsx
+++ part01/part1/src/App.jsx
@@ -2,22 +2,57 @@ import { useState } from "react";
 
 const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
+
+  const [value, setValue] = useState(0);
+
+  const setToValue = (value) => {
+    console.log("value now", value);
+    setValue(value);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {left}
+      <Button text="left" handleClick={handleLeftClick} />
+      <Button text="right" handleClick={handleRightClick} />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
+      <div>
+        {value}
+        <Button handleClick={() => setToValue(1000)} text="thousand" />
+        <Button handleClick={() => setToValue(0)} text="reset" />
+        <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      </div>
     </div>
   );
 };
