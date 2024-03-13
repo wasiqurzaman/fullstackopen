2aee5bf605d2653cef7ce27f10e67f3628b61f7a
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-13 18:31:19 +0530

    notes-example: changes to follow along

--- part02/notes-example/src/App.jsx
+++ part02/notes-example/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -19,10 +20,9 @@ const App = () => {
   // useEffect(hook, []);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((res) => {
-      console.log("promise fulfilled");
-      setNotes(res.data);
+    // console.log("effect");
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
   console.log("render", notes.length, "notes");
@@ -32,14 +32,35 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: notes.length + 1,
+      id: String(notes.length + 1),
     };
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+
+    noteService.create(noteObject).then((returnedNote) => {
+      // console.log(returnedNote);
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    // console.log("importance of ", id, " needs to be toggled");
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((note) => note.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note "${note.content}" was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -54,7 +75,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
--- part02/notes-example/src/components/Note.jsx
+++ part02/notes-example/src/components/Note.jsx
@@ -1,5 +1,14 @@
-const Note = ({ note }) => {
-  return <li>{note.content}</li>;
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? "make not important" : "make important";
+
+  return (
+    <div>
+      <li>
+        {note.content}
+        <button onClick={toggleImportance}>{label}</button>
+      </li>
+    </div>
+  );
 };
 
 export default Note;
