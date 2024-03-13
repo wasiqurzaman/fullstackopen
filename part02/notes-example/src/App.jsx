da5fe394c0bef18396d4fa8ddf04a435184cebf3
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-13 23:54:30 +0530

    notes-example: changes to follow along

--- part02/notes-example/src/App.jsx
+++ part02/notes-example/src/App.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 import Note from "./components/Note";
 import noteService from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
   // const hook = () => {
   //   console.log("effect");
@@ -58,7 +60,12 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
       .catch((error) => {
-        alert(`the note "${note.content}" was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content} was already removed from server'`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -68,6 +75,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -86,6 +94,7 @@ const App = () => {
         <input type="text" value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
--- part02/notes-example/src/components/Note.jsx
+++ part02/notes-example/src/components/Note.jsx
@@ -3,7 +3,7 @@ const Note = ({ note, toggleImportance }) => {
 
   return (
     <div>
-      <li>
+      <li className="note">
         {note.content}
         <button onClick={toggleImportance}>{label}</button>
       </li>
