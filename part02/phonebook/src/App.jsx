fbde0021d6ebd5f5ec47171358406feb13dbb778
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-13 15:04:17 +0530

    phonebook: exercise 2.11 -- step6 done

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import ShowPersons from "./components/ShowPersons";
+import axios from "axios";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  console.log("render", persons.length, "persons");
 
   const addPerson = (event) => {
     event.preventDefault();
