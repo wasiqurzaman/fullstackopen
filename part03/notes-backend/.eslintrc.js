2ed9bb4c0ecf5b1ee3a1931a45207f9054aebcf4
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-19 00:46:26 +0530

    notes-backend: add validation and linting

---
+++ part03/notes-backend/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	env: {
+		node: true,
+		commonjs: true,
+		es2021: true,
+	},
+	plugins: ["@stylistic/js"],
+	extends: "eslint:recommended",
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	rules: {
+		eqeqeq: "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": ["error", "always"],
+		"arrow-spacing": ["error", { before: true, after: true }],
+		"no-console": 0,
+		indent: [
+			"error",
+			2,
+			// 'tab'
+		],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+};
--- part03/notes-backend/index.js
+++ part03/notes-backend/index.js
@@ -1,26 +1,22 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 // const mongoose = require("mongoose");
-const Note = require('./models/note');
-const e = require('express');
-
+const Note = require("./models/note");
 
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 
-
-
 // middleware for logging
 const requestLogger = (request, response, next) => {
-  console.log('Method: ', request.method);
-  console.log('Path: ', request.path);
-  console.log('Body: ', request.body);
+  console.log("Method: ", request.method);
+  console.log("Path: ", request.path);
+  console.log("Body: ", request.body);
   console.log("----------------------");
   next();
-}
+};
 
 app.use(requestLogger);
 
@@ -31,11 +27,10 @@ app.get("/", (request, response) => {
 app.get("/api/notes", (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes);
-  })
-})
+  });
+});
 
-
-app.get('/api/notes/:id', (request, response, next) => {
+app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then(note => {
       if (note) {
@@ -46,33 +41,26 @@ app.get('/api/notes/:id', (request, response, next) => {
     })
     .catch(error => {
       next(error);
-      // console.log(error);
-      // response.status(400).send({ error: 'malformatted id' });
-    })
+    });
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  })
+  });
 
   note.save().then(savedNote => {
     response.json(savedNote);
   })
+    .catch(error => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
+      console.log(result);
       response.status(204).end();
     })
     .catch(error => next(error));
@@ -80,40 +68,37 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
+  const note = { content, important, };
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: "query" })
     .then(updatedNote => {
       response.json(updatedNote);
     })
     .catch(error => next(error));
-})
-
+});
 
 // unknown endpoint handler middleware
 const unknownEndpoint = (req, res, next) => {
-  res.status(404).send({ error: 'unkown endpoint' });
-}
+  res.status(404).send({ error: "unkown endpoint" });
+  next();
+};
 
 app.use(unknownEndpoint);
-
 
 // Error handler middleware
 const errorHandler = (error, request, response, next) => {
   console.error(error);
 
   if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" })
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
-}
+};
 
-// this has to be the last loaded middleware, also all the routes should be registered before this. 
+// this has to be the last loaded middleware, also all the routes should be registered before this.
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
